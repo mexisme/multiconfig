00e9b2facf658323ab42587abb6ac0d9f9e6bb77
mexisme <wildjim+dev@kiwinet.org>
2018-07-11 20:19:01 +1200

    Initial-add of unmarshaling layer

---
+++ marshal/unmarshal.go
@@ -0,0 +1,72 @@
+package marshal
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/subosito/gotenv"
+	// "github.com/hashicorp/hcl"
+	// "github.com/magiconair/properties"
+	toml "github.com/pelletier/go-toml"
+	// "github.com/spf13/cast"
+	"gopkg.in/yaml.v2"
+)
+
+func (s *Config) mapToConfigMap(tree interface{}) ConfigMap {
+	parsed := make(ConfigMap)
+
+	switch tree.(type) {
+	case gotenv.Env:
+		for k, v := range tree.(gotenv.Env) {
+			parsed[k] = v
+		}
+	case map[string]interface{}:
+		for k, v := range tree.(map[string]interface{}) {
+			parsed[k] = fmt.Sprintf("%v", v)
+		}
+	}
+
+	return parsed
+}
+
+func (s *Config) unmarshallIntoParsed() error {
+	extn := strings.ToLower(filepath.Ext(s.path))
+
+	switch extn {
+	case ".env":
+		env, err := gotenv.StrictParse(strings.NewReader(s.body))
+		if err != nil {
+			return s.ParseError(err)
+		}
+		s.parsed = s.mapToConfigMap(env)
+
+	case ".toml":
+		tree, err := toml.Load(s.body)
+		if err != nil {
+			return s.ParseError(err)
+		}
+
+		s.parsed = s.mapToConfigMap(tree.ToMap())
+
+	case ".yaml", ".yml":
+		if err := yaml.Unmarshal([]byte(s.body), &s.parsed); err != nil {
+			return s.ParseError(err)
+		}
+
+	case ".json":
+		var tree map[string]interface{}
+		if err := json.Unmarshal([]byte(s.body), &tree); err != nil {
+			return s.ParseError(err)
+		}
+
+		s.parsed = s.mapToConfigMap(tree)
+
+	// case ".properties", ".props", ".prop":
+	default:
+		return UnsupportedFormatError(extn)
+	}
+
+	return nil
+}
---
+++ marshal/unmarshal_test.go
@@ -0,0 +1,16 @@
+package marshal_test
+
+import (
+	// TODO: Add back when tests are written
+	// . "github.com/mexisme/multiconfig/marshal"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("multiconfig/marshal", func() {
+	It("Placeholder", func() {
+		Skip("No tests")
+		Expect(true).To(BeTrue())
+	})
+})
